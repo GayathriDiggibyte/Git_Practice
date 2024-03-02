e8dfdd2c497c13de1c7464c774f116635c729092
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-02 11:00:27 +0530

    sum

---
+++ sumofelements.py
@@ -0,0 +1,16 @@
+def sumofelements(l):
+    sum=0
+    for i in range(len(l)):
+        sum=sum+l[i]
+    return sum
+
+def main():
+    a=input("Enter the elements of the list separated by space\n")
+    ele=[]
+    l=[]
+    ele=a.split(" ")
+    for i in range(len(ele)):
+        l.append(int(ele[i]))
+    res=sumofelements(l)
+    print("The sum of the elements is : ",res)
+main()
