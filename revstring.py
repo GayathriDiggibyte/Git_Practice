90c2a7f16cad9481a96397ac37a7bf70c32309a2
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-02 15:30:35 +0530

    changes in the reversed string

--- revstring.py
+++ revstring.py
@@ -2,6 +2,10 @@ def rev(s):
   rev1=""
   for i in range(len(s)-1,-1,-1):
     rev1=rev1+s[i]
-  print(rev1)
-s="gaythri"
-rev(s)
+  return rev1
+
+def main():
+  s=input("Enter a string : \n")
+  res=rev(s)
+  print("The reversed string ",s," is : ",res)
+main()
