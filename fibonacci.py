ebd32019447aafd02eb7b8b2a41f29cc94108fe3
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-03 20:02:08 +0530

    fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fib(n):
+  t1=0
+  t2=1
+  print(t1," ",t2)
+  for i in range(2,n+1):
+    t3=t1+t2
+    print(t3)
+    t2=t3
+    t1=t2
+n=int(input("Enter a number to print fibonacci series: \n"))
+fib(n)
