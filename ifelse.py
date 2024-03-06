ba4805c2c293356a15157787b91c08ba4f4ea804
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-06 18:52:25 +0530

    if else statements

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number to compare: "))
+b=int(input("Enter a number to compare : "))
+if(a>b):
+    print(a," is greater than ",b)
+elif(a==b):
+    print(a," is equal to ",b)
+else:
+    print(a," is lesser than ",b)
