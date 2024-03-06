741aea7360973be1d60eeeefc588a4bd622b9b90
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-06 13:21:17 +0530

    Relational Operators

---
+++ Relational_Operator.py
@@ -0,0 +1,20 @@
+AgeOfGayu=22
+AgeOfChandu=23
+AgeOfLakshu=22
+print("Age of Gayu is :",AgeOfGayu)
+print("Age of Chandu is :",AgeOfChandu)
+print("Age of Lakshu is :",AgeOfLakshu)
+print("Checking AgeOfGayu == AgeOfChandu ?",AgeOfGayu == AgeOfChandu)
+print("Checking AgeOfGayu == AgeOfLakshu ?",AgeOfGayu == AgeOfLakshu)
+print("Checking AgeOfGayu != AgeOfChandu ?",AgeOfGayu != AgeOfChandu)
+print("Checking AgeOfGayu != AgeOfLakshu ?",AgeOfGayu != AgeOfLakshu)
+print("Checking AgeOfGayu > AgeOfChandu ?",AgeOfGayu > AgeOfChandu)
+print("Checking AgeOfGayu > AgeOfLakshu ?",AgeOfGayu > AgeOfLakshu)
+print("Checking AgeOfGayu < AgeOfChandu ?",AgeOfGayu < AgeOfChandu)
+print("Checking AgeOfGayu < AgeOfLakshu ?",AgeOfGayu < AgeOfLakshu)
+print("Checking AgeOfGayu >= AgeOfChandu ?",AgeOfGayu >= AgeOfChandu)
+print("Checking AgeOfGayu >= AgeOfLakshu ?",AgeOfGayu >= AgeOfLakshu)
+print("Checking AgeOfGayu <= AgeOfChandu ?",AgeOfGayu <= AgeOfChandu)
+print("Checking AgeOfGayu <= AgeOfLakshu ?",AgeOfGayu <= AgeOfLakshu)
+
+
