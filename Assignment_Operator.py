c4402413f52abe7b8e64fa33e4236470f872e096
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-06 14:34:38 +0530

    changes

--- Assignment_Operator.py
+++ Assignment_Operator.py
@@ -1,14 +1,30 @@
 a=10
 b=5
+print("a: ",a)
+print("b: ",b)
 a+=b
-print("a+=b",a)
+print("a+=b => ",a)
+a=10
+b=5
 a-=b
-print("a-=b",a)
+print("a-=b => ",a)
+a=10
+b=5
 a*=b
-print("a*=b",a)
+print("a*=b=> ",a)
+a=10
+b=5
 a/=b
-print("a/=b",a)
+print("a/=b => ",a)
+a=10
+b=5
 a//=b
-print("a//=b",a)
+print("a//=b => ",a)
+a=10
+b=5
 a%=b
-print("a%=b",a)
+print("a%=b => ",a)
+a=10
+b=5
+a**=b
+print("a**=b => ",a)
