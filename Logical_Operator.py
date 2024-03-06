d60618e7755acd76a5a3b21b5a9f98c8438d514d
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-06 13:31:40 +0530

    Logical Operator

---
+++ Logical_Operator.py
@@ -0,0 +1,16 @@
+a=5
+b=10
+print("a : ",a)
+print("b : ",b)
+print("a==5 and b==10: ",a==5 and b==10)
+print("a==5 and b==7: ",a==5 and b==7)
+print("a==4 and b==10: ",a==4 and b==10)
+print("a==4 and b==7: ",a==4 and b==7)
+print("a==5 or b==10: ",a==5 or b==10)
+print("a==5 or b==7: ",a==5 or b==7)
+print("a==4 or b==10: ",a==4 or b==10)
+print("a==4 or b==7: ",a==4 or b==7)
+print("not a==5",not (a==5))
+print("not a==7",not (a==7))
+
+
