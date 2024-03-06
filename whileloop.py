30c680edb15b3bb5f9a0fd8d2d1cafb4c40397de
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-06 19:07:05 +0530

    while loop

---
+++ whileloop.py
@@ -0,0 +1,20 @@
+#normal while loop
+i=1
+while i<6:
+    print(i)
+    i+=1
+
+# while with break
+j=1
+while j<6:
+    print(j)
+    if j==3:
+        break
+    j=j+1
+# while with continue
+k=1
+while k<6:
+    print(k)
+    if k==3:
+        continue
+    k=k+1
