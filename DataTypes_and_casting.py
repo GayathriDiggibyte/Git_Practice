38f9ea34a4987547618fa62bdda5696585a6f942
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-06 15:18:18 +0530

    Data Types and casting

---
+++ DataTypes_and_casting.py
@@ -0,0 +1,54 @@
+a=10
+b="Gayathri"
+c=345.56
+d=True
+complex1=3+5j
+l=["Apple","Orange","Cherry"] #list
+t=("Apple","Orange","Cherry") #tuple
+r=range(5) #range
+dict={"Name":"Gayathri","Age":22} #Dictionary
+set1={"Red","Violet","Blue"} #set
+frozenset1=frozenset({"Yellow","Green","Grey"}) #frozenset
+byte1=b"Hello"
+byteArray1=bytearray(5)
+memoryView=memoryview(bytes(5))
+nonetype=None
+type_case1=str(a)
+type_case2=int(c)
+type_case3=float(a)
+type_case4=str(d)
+type_case5=str(complex1)
+concat_str=type_case1+" fruits"
+print(type_case1,type_case2,type_case3,type_case4,type_case5)
+print(concat_str)
+print(a,b,c,d,complex1)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(complex1))
+print(l)
+print(type(l))
+print(t)
+print(type(t))
+print(r)
+print(type(r))
+print(dict)
+print(type(dict))
+print(set1)
+print(type(set1))
+print(frozenset1)
+print(type(frozenset1))
+print(byte1)
+print(type(byte1))
+print(byteArray1)
+print(type(byteArray1))
+print(memoryView)
+print(type(memoryView))
+print(nonetype)
+print(type(nonetype))
+print(type(type_case1))
+print(type(type_case2))
+print(type(type_case3))
+print(type(type_case4))
+print(type(type_case5))
