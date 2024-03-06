523450d7d24ce9fa9c3281fad930a0bccc10f7a8
GayathriMDiggi <mgayathri1192001@gmail.com>
2024-03-06 17:49:03 +0530

    Print Duplicated Elements change

--- PrintDuplicatedElements.py
+++ PrintDuplicatedElements.py
@@ -1,15 +1,10 @@
 l=[3,4,5,3,4,4,5,5,6,6,0,0,20,14,5,1,20]
 unique=[]
-duplicates=[]
+duplicates=set()
 for x in l:
     if x in unique:
-        duplicates.append(x)
+        duplicates.add(x)
     else:
         unique.append(x)
-for y in unique:
-    count=1
-    for i in range(len(duplicates)):
-        if(y == duplicates[i]):
-            count+=1
-    if(count == 2):
-        print(y)
+
+print(list(duplicates))
